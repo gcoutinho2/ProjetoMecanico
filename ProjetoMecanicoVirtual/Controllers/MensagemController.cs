14c56820ee1d1f5a9892aad32e3db62b40f9ed3e
Amauri Junior <junior.tw@hotmail.com>
2018-05-30 22:30:04 -0300

    Amauri fazendo cagada

---
+++ ProjetoMecanicoVirtual/Controllers/MensagemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoMecanicoVirtual.Controllers
+{
+    public class MensagemController : Controller
+    {
+        // GET: Mensagem
+        public ActionResult Mensagem()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjetoMecanicoVirtual/Controllers/VeiculoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoMecanicoVirtual.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        // GET: CadastroVeiculo
+        public ActionResult ConsultarInformacao()
+        {
+            return View();
+        }
+
+        public ActionResult CadastroVeiculo()
+        {
+            return View();
+        }
+    }
+}
