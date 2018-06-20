10ba4846f99a28f5981f3aeff642d9204310cb1b
Coutinho <gabriel.souza@resourceit.com>
2018-06-20 14:29:08 -0300

    Ajustes montadora, marca e veículo

--- ProjetoMecanicoVirtual/DAO/MontadoraDAO.cs
+++ ProjetoMecanicoVirtual/DAO/MontadoraDAO.cs
@@ -7,19 +7,20 @@ using System.Web;
 
 namespace ProjetoMecanicoVirtual.DAO
 {
-    public class MarcaDAO
+    public class MontadoraDAO
     {
-        public void InserirMarca(Marca marca)
+        public void InserirMontadora(Montadora montadora)
         {
             using (SqlConnection conn = Conexao.AbrirConexao())
             {
-                using (SqlCommand cmd = new SqlCommand("INSERT INTO MODELO (nome) VALUES (@nome)", conn))
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO MONTADORA (nome) VALUES (@nome)", conn))
                 {
-                    cmd.Parameters.AddWithValue("@nome", marca.Nome);
+                    cmd.Parameters.AddWithValue("@nome", montadora.Nome);
 
                     cmd.ExecuteNonQuery();
                 }
             }
+
         }
     }
 }
--- ProjetoMecanicoVirtual/Models/Montadora.cs
+++ ProjetoMecanicoVirtual/Models/Montadora.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace ProjetoMecanicoVirtual.Models
 {
-    public class Marca
+    public class Montadora
     {
         public int Id { get; set; }
 
